86b9d4251c94b5a7cb57d62233317922075f814d
Henry <henryli76@gmail.com>
2019-10-19 18:07:23 -0400

    <!DOCTYPE html> & Enumeration.hasMoreElements

--- src/first/HelloWorldServlet.java
+++ src/first/HelloWorldServlet.java
@@ -2,6 +2,7 @@ package first;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,31 +16,50 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloWorldServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloWorldServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("<h1>Welcome, My Friend!</h1>");
-		PrintWriter out = response.getWriter();
-		out.println("Nothing"+request.getContextPath());
-		out.close();		
+		
+//		  response.getWriter().append("Served at: ").append(request.getContextPath());
+//		  response.getWriter().append("<h1>Welcome, My Friend!</h1>"); 
+		PrintWriter out = response.getWriter(); 
+//		  out.println("Nothing"+request.getContextPath());
+		
+		 
+		Enumeration<String> params = request.getParameterNames();
+		if (!params.hasMoreElements()) out.println("No Parameters");
+		else {
+			out.println("<!DOCTYPE html>");
+			out.println("<ul>");
+			
+			while (params.hasMoreElements())  {
+				String variableName=params.nextElement();
+				out.println("<li>"+variableName + ":"+request.getParameter(variableName)+"</li>");
+			} 
+			out.println("</ul>");
+			out.println("</html>");
+		}
+		
+		out.close();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
